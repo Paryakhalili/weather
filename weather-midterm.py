d6877ac0d7bbe5c6062c4fe8bfef7814e5ec6e87
Parya <137226899+Paryakhalili@users.noreply.github.com>
2023-09-06 15:22:49 +0330

    Update weather-midterm.py

--- weather-midterm.py
+++ weather-midterm.py
@@ -1,101 +1,94 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
 import requests
 import pandas as pd
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
+from prettytable import PrettyTable
 
 
-# In[81]:
-
-
+# City list
 cities = [
-    ["Tehran", "Iran"], 
-    ["Ottawa", "Canada"], 
-    ["Seoul", "South Korea"], 
-    ["Madrid", "Spain"], 
-    ["Tokyo", "Japan"],
-    ["Kyiv", "Ukraine"],
-    ["Warsaw", "Poland"],
-    ["Berlin", "Germany"],
-    ["London", "UK"],
-    ["Madrid", "Spain"],
-    ["Paris", "France"],
-    ["Rome", "Italy"],
-    ["Prague", "Czechia"],
-    ["Istanbul", "Turkey"],
-    ["Stockholm", "Sweden"],
-    ["Sofia", "Bulgaria"],
-    ["Bucharest", "Romania"],
-    ["Zurich", "Switzerland"],
+    "Tehran, Iran",
+    "Ottawa, Canada",
+    "Seoul, South Korea",
+    "Madrid, Spain",
+    "Tokyo, Japan",
+    "Kyiv, Ukraine",
+    "Warsaw, Poland",
+    "Berlin, Germany",
+    "London, UK",
+    "Madrid, Spain",
+    "Paris, France",
+    "Rome, Italy",
+    "Prague, Czechia",
+    "Istanbul, Turkey",
+    "Stockholm, Sweden",
+    "Sofia, Bulgaria",
+    "Bucharest, Romania",
+    "Zurich, Switzerland",
 ]
-df = pd.DataFrame(cities, columns=["city", "country"])
+
+# Create dataframe
+df = pd.DataFrame(cities, columns=["city"])
+
+# Function for coordinates
+def get_coordinates(city):
+    locator = Nominatim(user_agent="myGeocoder")
+    location = locator.geocode(city)
+    if location:
+        return {
+            "latitude": location.latitude,
+            "longitude": location.longitude
+        }
+    else:
+        return {
+            "latitude": None,
+            "longitude": None
+        }
+
+# Function for weathers data
+def get_weather(latitude, longitude):
+    api_key = "YOUR_API_KEY"  # Enter your Api key
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}"
+    response = requests.get(url)
+    data = response.json()
+    if "main" in data and "weather" in data:
+        return {
+            "temp": data["main"]["temp"] - 273.15,
+            "pressure": data["main"]["pressure"],
+            "humidity": data["main"]["humidity"],
+            "description": data["weather"][0]["description"],
+            "icon": data["weather"][0]["icon"],
+        }
+    else:
+        return {
+            "temp": None,
+            "pressure": None,
+            "humidity": None,
+            "description": None,
+            "icon": None,
+        }
+
+# Function for Air Polution
+def get_air_pollution(latitude, longitude):
+    api_key = "YOUR_API_KEY"  #Enter your Api key
+    url = f"https://api.openweathermap.org/data/2.5/air_pollution?lat={latitude}&lon={longitude}&appid={api_key}"
+    response = requests.get(url)
+    data = response.json()
+    if "list" in data and len(data["list"]) > 0 and "main" in data["list"][0]:
+        return {
+            "aqi": data["list"][0]["main"]["aqi"]
+        }
+    else:
+        return {
+            "aqi": None
+        }
 
 
-# In[88]:
+# Create new culomns for weather data
+df["coordinates"] = df["city"].apply(get_coordinates)
+df["latitude"] = df["coordinates"].apply(lambda x: x["latitude"])
+df["longitude"] = df["coordinates"].apply(lambda x: x["longitude"])
+df["weather"] = df.apply(lambda row: get_weather(row["latitude"], row["longitude"]), axis=1)
+df["air_quality"] = df.apply(lambda row: get_air_pollution(row["latitude"], row["longitude"]), axis=1)
 
-
-locator = Nominatim(user_agent="myGeocoder")
-geocode = RateLimiter(locator.geocode, min_delay_seconds=.1)
-
-def get_coordinates(city, country):
-  response = geocode(query={"city": city, "country": country})
-  return {
-    "latitude": response.latitude,
-    "longitude": response.longitude
-  }
-
-df_coordinates = df.apply (lambda x: get_coordinates(x.city, x.country), axis=1)
-df = pd.concat([df, pd.json_normalize(df_coordinates)], axis=1)
-
-
-# In[91]:
-
-
-import datetime
-
-def get_weather(row):
-
-  url = "https://api.openweathermap.org/data/2.5/weather?lat={row.latitude}&lon={row.longitude}&appid={my API Key}"
-  my_response = requests.get(url)
-  data = my_response.json()
-
-  return {
-      "temp": data["main"]["temp"] - 273.15,
-      "pressure": data["weather"][0]["pressure"],
-      "humidity":data["weather"][0]["humidity"],
-      "description": data["weather"][0]["description"],
-      "icon": data["weather"][0]["icon"],
-  }
-
-df_weather = df.apply (lambda x: get_weather(x), axis=1)
-df = pd.concat ([df, pd.json_normalize(df_weather)], axis=1)
-
-
-
-# In[94]:
-
-
-def get_air_pollution(row):
-
-  url = "http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={API key}?lat={row.latitude}&lon={row.longitude}&appid={my API Key}"
-  my_response = requests.get(url)
-  data = my_response.json()
-
-  return {
-      "temp": data["main"]["aqi"]
-  }
-
-df_air_pollution = df.apply (lambda x: get_air_pollution(x), axis=1)
-df = pd.concat ([df, pd.json_normalize(df_air_pollution)], axis=1)
-
-
-# In[ ]:
-
-
-
-
+print (df)
